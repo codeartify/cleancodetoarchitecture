8ca809f30857de444c8ce6e007a7eb97acfbe669
Oliver Zihler <github@codeartify.com>
2025-08-24 11:24:50 +0200

    first commit

---
+++ src/test/java/com/codeartify/cleancodetoarchitecture/CleanCodeToArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeartify.cleancodetoarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCodeToArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
