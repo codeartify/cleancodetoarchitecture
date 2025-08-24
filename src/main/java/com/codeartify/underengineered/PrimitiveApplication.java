d67c04c7197792859b4936a8b1599c90bf88fe7e
Oliver Zihler <github@codeartify.com>
2025-08-24 12:30:15 +0200

    Add checker

--- src/main/java/com/codeartify/underengineered/PrimitiveApplication.java
+++ src/main/java/com/codeartify/underengineered/PrimitiveApplication.java
@@ -1,4 +1,4 @@
-package com.codeartify.primitive;
+package com.codeartify.underengineered;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
