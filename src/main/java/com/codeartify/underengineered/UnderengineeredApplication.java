a397902d54d3619eff95bddb585cd40ee89d79c4
Oliver Zihler <github@codeartify.com>
2025-08-31 13:25:23 +0200

    separate apps

--- src/main/java/com/codeartify/underengineered/UnderengineeredApplication.java
+++ src/main/java/com/codeartify/underengineered/UnderengineeredApplication.java
@@ -4,15 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Map;
+
 @SpringBootApplication
 public class UnderengineeredApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(UnderengineeredApplication.class);
-        app.setDefaultProperties(Map.of(
-                "server.port", "8080",
-                "spring.application.name", "underengineered"
-        ));
+        app.setAdditionalProfiles("underengineered");
         app.run(args);
     }
 }
-
