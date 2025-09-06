e39edf24d1936971900bd4b562d10ad11a486ed9
Oliver Zihler <github@codeartify.com>
2025-09-06 11:40:46 +0200

    rename

--- src/main/java/com/codeartify/underengineered/Controller.java
+++ src/main/java/com/codeartify/underengineered/Controller.java
@@ -42,7 +42,7 @@ public class Controller {
             throw new RuntimeException("radius must be greater than 0");
         }
 
-        jdbcTemplate.query("SELECT id, x, y FROM properties", rs -> {
+        jdbcTemplate.query("SELECT id, x, y FROM real_estate", rs -> {
             var id = rs.getLong("id");
             var xx = rs.getDouble("x");
             var yy = rs.getDouble("y");
